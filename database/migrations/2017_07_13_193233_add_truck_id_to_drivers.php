75c76fb0a72fe25f139e6f3c1c9c78ff6db529ef
Juan Carlos <juancagb.17@gmail.com>
2017-07-13 19:48:30 -0500

    InnoDB

--- database/migrations/2017_07_13_193233_add_truck_id_to_drivers.php
+++ database/migrations/2017_07_13_193233_add_truck_id_to_drivers.php
@@ -27,7 +27,7 @@ class AddTruckIdToDrivers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropForeign('truck_id');
+            $table->dropColumn('truck_id');
         });
     }
 }
